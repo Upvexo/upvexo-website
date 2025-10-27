a96269e49f980428755d3f5b3c6f745d23644ddf
Pasachiya <pasachiya@gmail.com>
2025-10-27 14:54:03 +0530

    Update jsconfig.json to exclude 'out' directory and modify next.config.js to reset basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/upvexo-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/upvexo-website/' : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
