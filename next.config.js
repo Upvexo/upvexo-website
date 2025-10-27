fd13554fc45e10ff87945a694d5a1ad328988ade
Pasachiya <pasachiya@gmail.com>
2025-10-27 14:51:50 +0530

    Refactor Next.js configuration and add .nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/upvexo-website' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/upvexo-website/' : '',
-    reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/upvexo-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/upvexo-website/' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
