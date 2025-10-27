ae20e31fde8ba2d6b3a3cf723a0946f232413290
Pasachiya <pasachiya@gmail.com>
2025-10-27 15:06:28 +0530

    Reset basePath and assetPrefix in next.config.js for development environment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/upvexo-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/upvexo-website' : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
 }
 
-module.exports = nextConfig
 module.exports = nextConfig
